242413c019a0b907fc4a980ece73301b30dd64bf
Baudoin <baudoinvicbolingo@gmail.com>
2025-07-10 16:56:11 +0200

    fixed the error on footer on deployement

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaFacebook } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import logo from "../assets/logo.PNG";
+
 
 const Footer = () => {
   return (
