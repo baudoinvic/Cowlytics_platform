94b5fa32009184d0996caccf44ff3ab70a843739
Baudoin <baudoinvicbolingo@gmail.com>
2025-07-10 16:51:34 +0200

    fixed the error

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 
 import { Users } from "lucide-react";
-import logo from "../assets/logo.PNG";
+import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
